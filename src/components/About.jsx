ff5bee5626d51d0a0920875f6ffac4144da55e92
bharath <bwaj9078@gmail.com>
2024-10-13 12:49:00 +0530

    portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white ">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full ">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+
+        <p className="text-xl mt-20">
+          As a dedicated Frontend Developer with over 1.2 years of experience, I
+          specialize in creating intuitive and engaging user interfaces using a
+          robust foundation in web technologies. My technical expertise spans
+          HTML, CSS, JavaScript, and React, where I excel in developing seamless
+          and responsive designs that prioritize user experience. I have a
+          proven track record of building diverse applications that align with
+          business objectives, leveraging my deep understanding of UI principles
+          and modern development practices.
+        </p>
+        <br />
+        <p className="text-xl ">
+          In addition to my core skills, I have hands-on experience with
+          Material-UI (MUI) and Supabase, which I use to create dynamic,
+          scalable solutions that meet the evolving needs of clients. My focus
+          on frontend development has enabled me to deliver high-quality,
+          tailored applications that drive engagement and enhance functionality.
+          I am passionate about continuously expanding my skill set and staying
+          up-to-date with the latest industry trends to provide innovative
+          solutions in the ever-changing landscape of web development.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Home.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import HeroImage from "../assets/Bharath.png";
+import { MdKeyboardArrowRight } from "react-icons/md";
+import { Link } from "react-scroll";
+
+export const Home = () => {
+  return (
+    <div
+      name="home"
+      className=" h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 ">
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row  ">
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-3xl sm:text-3xl font-bold text-white">
+            I'm a Frontend Developer
+          </h2>
+          <p className="text-gray-500 py-4 max-w-md">
+            Frontend Developer with 1.2 years of experience specializing in
+            HTML, CSS, JavaScript, React, MUI, and Supabase. Skilled in
+            developing tailored applications that meet business requirements.
+          </p>
+          <div>
+            <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer ">
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300">
+                <MdKeyboardArrowRight size={25} className="ml-1" />
+              </span>
+            </Link>
+          </div>
+        </div>
+        <div>
+          <img
+            src={HeroImage}
+            alt="my profile"
+            className="rounded-2xl mx-auto  w-3/4 md:w-2/3 lg:w-1/2  max-h-[450px]"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
